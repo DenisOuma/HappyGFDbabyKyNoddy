0997398d208927839cc029214ec00b620f250e16
DenisOuma <dennisouma38@gmail.com>
2021-07-31 19:37:46 +0300

    Finished

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function heart(){
     const heart = document.createElement("div");
     heart. classList.add("heart");
     heart.innerText = "💖";
+    
     heart.style.left=Math.random()*100 + "vw";
     heart.style.animationDuration=Math.random()*2+3;
 
